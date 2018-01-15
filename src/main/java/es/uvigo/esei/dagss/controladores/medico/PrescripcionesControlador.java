2abe7a37c39e9cec134444e739a624b2c22779b2
eldam <mbmartin@esei.uvigo.es>
2018-01-15 18:44:32 -0500

    Add "loadPrescripciones" in "prescripciones" controller

--- src/main/java/es/uvigo/esei/dagss/controladores/medico/PrescripcionesControlador.java
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/PrescripcionesControlador.java
@@ -5,13 +5,57 @@
  */
 package es.uvigo.esei.dagss.controladores.medico;
 
+import es.uvigo.esei.dagss.dominio.daos.PrescripcionDAO;
+import es.uvigo.esei.dagss.dominio.entidades.Paciente;
+import es.uvigo.esei.dagss.dominio.entidades.Prescripcion;
 import java.io.Serializable;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named(value = "prescripcionesControlador")
 @SessionScoped
 public class PrescripcionesControlador implements Serializable{
+    
+    @Inject
+    PrescripcionDAO prescripcionDAO;
+    
+    List<Prescripcion> prescripciones;
+    Prescripcion prescripcionActual;
+    
+    
+    public PrescripcionesControlador(){
+    }
+    
+    public List<Prescripcion> loadPrescripciones(Paciente paciente){
+        this.prescripciones = this.prescripcionDAO.getRecetasPaciente(paciente);
+        return this.prescripciones;
+    }
+
+    public PrescripcionDAO getPrescripcionDAO() {
+        return prescripcionDAO;
+    }
+
+    public void setPrescripcionDAO(PrescripcionDAO prescripcionDAO) {
+        this.prescripcionDAO = prescripcionDAO;
+    }
+
+    public List<Prescripcion> getPrescripciones() {
+        return prescripciones;
+    }
+
+    public void setPrescripciones(List<Prescripcion> prescripciones) {
+        this.prescripciones = prescripciones;
+    }
+
+    public Prescripcion getPrescripcionActual() {
+        return prescripcionActual;
+    }
+
+    public void setPrescripcionActual(Prescripcion prescripcionActual) {
+        this.prescripcionActual = prescripcionActual;
+    }
     
     
     
